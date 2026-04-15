7b17f582398fbe7236cb2fee21d6fe0da43a766f
karthikrajasrc <karthikrajasrc@gmail.com>
2026-04-15 10:25:01 +0530

    Cors udpated

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const commentRouter = require('./Routers/commentRouter');
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://moviesreviewe.netlify.app/","http://localhost:5173"],
     credentials: true
 }));
 
